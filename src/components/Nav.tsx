d8d59f103506ca18949c50e0794d92d16bfc6333
IvyTaff <ivy.taff@yahoo.com>
2025-02-27 22:56:26 -0600

    improved files

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-4 text-white">
+        <li>
+          <Link to="/" className="hover:underline">Home</Link>
+        </li>
+        <li>
+        <Link to="/potential-candidates" className="hover:underline">Potential Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/interface/Candidate.interface.tsx
+++ src/interface/Candidate.interface.tsx
@@ -1,1 +1,18 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface GitHubUser {
+    login: string;
+    id: number;
+    avatar_url: string;
+    html_url: string;
+  }
+  
+  interface GitHubUserDetails extends GitHubUser {
+    name?: string;
+    company?: string;
+    location?: string;
+    bio?: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    created_at: string;
+  }
